9b4eaa6e77d7a644eac37400ec7afbf4a25621cd
Ray Luong <rayluong@gmail.com>
2017-02-02 11:56:34 -0800

    change station names to abbrv

--- trains.js
+++ trains.js
@@ -1,118 +1,117 @@
-console.log('export trains');
 var coordinates = [{
   "red": [
       {
-        "station": "Richmond", 
+        "station": "RICH", 
         "x": 333, 
         "y": 120 
       },
       {
-        "station": "El Cerrito Del Norte", 
+        "station": "DELN", 
         "x": 344, 
         "y": 142 
       },
       {
-        "station": "El Cerrito Plaza", 
+        "station": "PLZA", 
         "x": 356, 
         "y": 164 
       },
       {
-        "station": "North Berkeley", 
+        "station": "NBRK", 
         "x": 367, 
         "y": 187 
       },
       {
-        "station": "Downtown Berkeley", 
+        "station": "DBRK", 
         "x": 378, 
         "y": 209 
       },
       {
-        "station": "Ashby", 
+        "station": "ASHB", 
         "x": 391, 
         "y": 234 
       },
       {
-        "station": "MacArthur", 
+        "station": "MCAR", 
         "x": 387, 
         "y": 269 
       },
       {
-        "station": "19th St", 
+        "station": "19TH", 
         "x": 384, 
         "y": 291 
       },
       {
-        "station": "12th St", 
+        "station": "12TH", 
         "x": 381, 
         "y": 318 
       },
       {
-        "station": "West Oakland", 
+        "station": "WOAK", 
         "x": 351, 
         "y": 318 
       },
       {
-        "station": "Embarcadero", 
+        "station": "EMBR", 
         "x": 230, 
         "y": 347 
       },
       {
-        "station": "Montgomery",
+        "station": "MONT",
         "x": 219,
         "y": 366 
       },
       {
-        "station": "Powell", 
+        "station": "POWL", 
         "x": 208, 
         "y": 385 
       },
       {
-        "station": "Civic Center", 
+        "station": "CIVC", 
         "x": 197, 
         "y": 404 
       },
       {
-        "station": "16th St", 
+        "station": "16TH", 
         "x": 184, 
         "y": 424 
       },
       {
-        "station": "24th St", 
+        "station": "24TH", 
         "x": 172, 
         "y": 445 
       },
       {
-        "station": "Glen Park", 
+        "station": "GLEN", 
         "x": 162, 
         "y": 462 
       },
       {
-        "station": "Balboa Park", 
+        "station": "BALB", 
         "x": 152, 
         "y": 479 
       },
       {
-        "station": "Daly City", 
+        "station": "DALY", 
         "x": 142, 
         "y": 497 
       },
       {
-        "station": "Colma", 
+        "station": "COLM", 
         "x": 147, 
         "y": 554 
       },
       {
-        "station": "South San Francisco", 
+        "station": "SSAN", 
         "x": 171, 
         "y": 590 
       },
       {
-        "station": "San Bruno", 
+        "station": "SBRN", 
         "x": 195, 
         "y": 623 
       },
       {
-        "station": "Millbrae", 
+        "station": "MLBR", 
         "x": 227, 
         "y": 669 
       }
