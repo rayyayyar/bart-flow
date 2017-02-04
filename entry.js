e7928282456874c8dfd1cf6a0a5264f227613029
Ray Luong <rayluong@gmail.com>
2017-02-03 17:13:07 -0800

    sketch trainload without exits

--- entry.js
+++ entry.js
@@ -8,7 +8,7 @@ ctx.lineWidth = 1;
 ctx.lineCap="round";
 import coordinates from "./trains.js";
 var riders = require("./data/riders.json");
-var trainLoad = 1;
+var trainLoad = null;
 
 // function showRiders(json) {
 //   console.log("showRiders()");
@@ -39,8 +39,11 @@ var trainLoad = 1;
 
 var redLine = coordinates[0].red;
 console.log("redLine:" + redLine);
-ctx.moveTo(redLine[0].x, redLine[0].y);
+var redLineRiders = riders.filter(isOnRed);
+console.log("redLineRiders: " + JSON.stringify(redLineRiders));
 
+
+ctx.moveTo(redLine[0].x, redLine[0].y);
 var s = 0;
 var duration = 1000;
 var startTime = null;
@@ -49,9 +52,21 @@ function matchTrip(origin, dest, t) {
   return t.origin == origin && t.dest == dest;
 }
 
-function isOrigin(origin, t) {
-  return t.origin == origin;
+function isSouthBound(t) {
+  // assumes coordinate data is always listed north to south
+  return getStationIndex(redLine, t.origin) < getStationIndex(redLine, t.dest);
 }
+
+function getStationIndex(line, station) {
+  var i = line.map(function(e) {
+    return e.station;
+  }).indexOf(station);
+  return i;
+}
+
+// filter down redLineRiders json to only Southbound trips
+var redLineRidersSouthBound = redLineRiders.filter(isSouthBound);
+console.log("redLineRidersSouthBound: " + JSON.stringify(redLineRidersSouthBound));
 
 function isOnRed(t) {
   // is trip object t's origin  == to any of redLine's stations?
@@ -66,14 +81,11 @@ function isOnRed(t) {
   }
 }
 
-var redLineRiders = riders.filter(isOnRed);
-console.log("redLineRiders: " + JSON.stringify(redLineRiders));
-
 // sum all riders that board at an origin station that's on a specific line
 function sumBoard (line, origin) {
+  console.log("sumBoard()");
   var ridersCount = line.reduce(function(sum, x) {
     if (x.origin == origin) {
-      console.log("sum");
       return sum + x.riders;
     }
     return sum;
@@ -81,34 +93,17 @@ function sumBoard (line, origin) {
   return ridersCount;
 }
 
-var sum12th = sumBoard(redLineRiders, "12TH");
-console.log("sum12th: " + sum12th);
-
 function animate(time) {
   var origin = redLine[s].station;
   var dest = redLine[s+1].station;
-  // create new array of data that matches current origin / dest pair
-  var tripMatch = riders.filter(matchTrip.bind(this, origin, dest));
-  // console.log("tripMatch: " + JSON.stringify(tripMatch));
-
-  if (tripMatch[0]) {
-    // trainLoad += sumBoard(redLine, origin);
-    console.log("sumBoard(redLineRiders, origin): " + sumBoard(redLineRiders, origin));
-    if (tripMatch[0].riders > trainLoad) {
-      trainLoad += tripMatch[0].riders;
-    }
-    else trainLoad -= sumBoard(redLineRiders, origin);
-    ctx.lineWidth = trainLoad;
-    console.log("trainLoad: " + trainLoad);
-  }
-
-	// console.log("redline current station:" + redLine[s].station);
- //  console.log("redline next station:" + redLine[s+1].station);
-	// console.log("time: " + time);
+  // create new object array with ridership that matches current origin / dest pair
+  var currTrip = riders.filter(matchTrip.bind(this, origin, dest));
+  console.log("currTrip: " + JSON.stringify(currTrip));
 
   if (!startTime) {
     startTime = time;
   }
+
   var timeElapsed = time - startTime;
   var delta = Math.min(1, timeElapsed / duration);
   var dX = (redLine[s+1].x - redLine[s].x) * delta;
@@ -118,11 +113,25 @@ function animate(time) {
   ctx.moveTo(Math.round(redLine[s].x), Math.round(redLine[s].y));
   ctx.lineTo(Math.round(redLine[s].x + dX), Math.round(redLine[s].y + dY));
   ctx.stroke();
-	
+
 	if (delta < 1) {
-    requestAnimationFrame(animate);    
+    // train's moving
+    requestAnimationFrame(animate);
   } else {
+    // train stops
     startTime = null;
+    console.log("origin: " + origin);
+    console.log(sumBoard(redLineRidersSouthBound, origin) + " boarded at " + origin);
+    trainLoad += sumBoard(redLineRidersSouthBound, origin);
+    console.log("trainLoad: " + trainLoad);
+    if (currTrip[0]) {
+      trainLoad -= currTrip[0].riders;
+      console.log(currTrip[0].riders + " exit at " + dest);
+      console.log("trainLoad -= currTrip[0].riders, trainLoad = " + trainLoad);
+      //console.log("trainLoad: " + trainLoad);
+      ctx.lineWidth = trainLoad / 250;
+    }
+    // look for total exits for this destination from all trips
     s++;
     setTimeout(function(){ startAnim() });
   }
