4462f46fa061a841f515bb4d23ce11e20c0a8f10
Ray Luong <rayluong@gmail.com>
2017-02-20 17:22:09 -0800

    psuedocode for massage

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ var svg = d3.select("#line")
   .attr("id", "visualization")
   .attr("xmlns", "http://www.w3.org/2000/svg");
 
+d3.select("body").style("background-color", "floralwhite");
 var queue = d3.queue();
 var red, blue, orange, yellow, green;
+var linesArray = [];
 var riders;
 var trainLoad = 0;
 var endCount = 0;
 var h = 0;
+var trips;
 var line =  d3.line()
   .x(function(d) { return d.x;  })
   .y(function(d) { return d.y; });
@@ -37,113 +40,114 @@ queue.awaitAll(function(error, jsonData) {
   yellow = jsonData[3];
   green = jsonData[4];
   riders = jsonData[5];
-  draw(red, h);
-  draw(orange, h);
-  draw(blue, h);
-  draw(yellow, h);
-  draw(green, h);
-  console.log("ready");
+  var redTrips = massage(red);
+  var blueTrips = massage(blue);
+  linesArray = [red, blue, orange, yellow, green];
+  var interval = setInterval(function() {
+    if (h >= 24) { clearInterval(interval); }
+    if (linesArray.length < 1) {
+      ++h;
+      document.getElementById("hour").innerHTML = h;
+      linesArray = [red, blue, orange, yellow, green];
+    }
+    var i = Math.floor(Math.random() * linesArray.length);
+    trips = massagedData(linesArray[i], h);
+    draw(linesArray[i], trips);
+    linesArray.splice(i, 1);
+
+  }, Math.min(Math.random() * 4000, 2500));
 });
 
-d3.select("body").style("background-color", "white");
 
-var timerCount = 0;
-var ms = 12500 / 60;
-function timer() {
-  if (timerCount < 60) {
-    document.getElementById("minutes").innerHTML = ":" + timerCount;
-    setTimeout(timer, ms);
-    ++timerCount;
-  }
-  else {
-    document.getElementById("minutes").innerHTML = ":" + "00";
-  }
-}
-timer();
+function massage(line) {
+  // store each set of trips matching specific line color, direction, and hour
+  // redTrips: [[red0Object], [red1Object], [red2Object],... , [red24Object]]
+  // when draw, call lineColor, then simply grab item in array of tripsets
+  // return item in array of tripsets
 
-function pad(n) {
-    return (n < 10) ? ("0" + n) : n;
-}
-
-function draw(lineColor, h) {
-  console.log("draw hour " + h);
-
-  var coordinates = lineColor;
-  console.log("coordinates: " + JSON.stringify(coordinates));
-  var temp = [];
-  var time = 0;
-  var totalTime = 0;
-  
+  var coordinates = line;
   // filter ridership data to only trips on a specific line (red, blue, green, etc)
   var lineRiders = riders.filter(isOnLine);
   // filter ridership data to direction NB or SB
   var lineRidersSB = lineRiders.filter(isSouthBound);
   var trips = lineRidersSB.filter(tripsHour(h));
+  return trips;
+  //returns trips for all hours on a line
 
-  function isSouthBound(t) {
-    // assumes coordinate data is always listed north to south
-    return getStationIndex(coordinates, t.origin) < getStationIndex(coordinates, t.dest);
-  }
+}
 
-  function getStationIndex(line, station) {
-    var i = line.map(function(e) {
-      return e.station;
-    }).indexOf(station);
-    return i;
-  }
+function isSouthBound(t) {
+  // assumes coordinate data is always listed north to south
+  return getStationIndex(coordinates, t.origin) < getStationIndex(coordinates, t.dest);
+}
 
-  function isOnLine(t) {
-    // is trip object t's origin  == to any of coordinates's stations?
-    for (var i = 0; i < coordinates.length; i++) {
-      if (t.origin == coordinates[i].station) {
-        for (var i = 0; i < coordinates.length; i++) {
-          if (t.dest == coordinates[i].station) {
-            return true;
-          }
+function getStationIndex(line, station) {
+  var i = line.map(function(e) {
+    return e.station;
+  }).indexOf(station);
+  return i;
+}
+
+function isOnLine(t) {
+  // is trip object t's origin  == to any of coordinates's stations?
+  for (var i = 0; i < coordinates.length; i++) {
+    if (t.origin == coordinates[i].station) {
+      for (var i = 0; i < coordinates.length; i++) {
+        if (t.dest == coordinates[i].station) {
+          return true;
         }
       }
     }
   }
+}
 
-  // sum all riders that board at an origin station that's on a specific line
-  function sumBoard (line, origin) {
-    var ridersCount = line.reduce(function(sum, x) {
-      if (x.origin == origin) {
-        return sum + x.riders;
-      }
-      return sum;
-    }, 1);
-    return ridersCount;
-  }
-
-  // for a set of trips, sum all riders that exit at a specified dest station
-  function sumExits (trips, dest) {
-    var ridersCount = trips.reduce(function(sum, x) {
-      if (x.dest == dest) {
-        return sum + x.riders;
-      }
-      return sum;
-    }, 1);
-    return ridersCount;
-  }
-
-  //filter function to return only trips of a requested hour
-  function tripsHour(hour) {
-    return function(t) {
-      return t.hour == hour;
+// sum all riders that board at an origin station that's on a specific line
+function sumBoard (line, origin) {
+  var ridersCount = line.reduce(function(sum, x) {
+    if (x.origin == origin) {
+      return sum + x.riders;
     }
+    return sum;
+  }, 1);
+  return ridersCount;
+}
+
+// for a set of trips, sum all riders that exit at a specified dest station
+function sumExits (trips, dest) {
+  var ridersCount = trips.reduce(function(sum, x) {
+    if (x.dest == dest) {
+      return sum + x.riders;
+    }
+    return sum;
+  }, 1);
+  return ridersCount;
+}
+
+// filter function to return only trips of a requested hour
+function tripsHour(hour) {
+  return function(t) {
+    return t.hour == hour;
   }
-  console.log("trips: " + JSON.stringify(trips));
+}
+
+function draw(lineColor, trips) {
+  console.log("draw hour " + h);
+  var coordinates = lineColor;
+  var temp = [];
+  var time = 0;
+  var totalTime = 0;
+
+  // massaged data returning 'trips' variable
 
   // loop through to draw each trip individually until the end of coordinates
   for(var i = 0; i < coordinates.length - 1; ++i) {
     temp[0] = coordinates[i];
     temp[1] = coordinates[i+1];
-    time = 500;
+    time = 900;
     var origin = temp[0].station;
     var dest = temp[1].station;
     trainLoad += sumBoard(trips, origin) - sumExits(trips, dest);
-    console.log(sumBoard(trips, origin) + " get on at " + origin + " and " + sumExits(trips, dest) + " get off at " + dest);
+    console.log("At " + h + ":00, " + sumBoard(trips, origin) + " get on at " + origin + " and " + sumExits(trips, dest) + " get off at " + dest);
     console.log("trainLoad: " + trainLoad);
     
     var paths = svg.append("path")
@@ -183,14 +187,12 @@ function draw(lineColor, h) {
           .ease(d3.easeLinear)
           .attr("stroke-dashoffset", 0)
           .on("end", function(d) {
-            console.log("n: " + n);
-            console.log("hour: " + h);
             d3.select(this)
               .transition()
                 .delay(0)
                 .duration(1000)
                 .ease(d3.easeLinear)
-                .attr("opacity", 0.01);
+                .attr("opacity", 0.018);
             ++n;
             // when all paths are drawn on a trainline
             if (n >= i) {
@@ -199,26 +201,11 @@ function draw(lineColor, h) {
           });
 
     totalTime += time;
+    // todo: animate analog clock based on time
   }
 
   function endAll(lineColor) {
-    console.log("this is the end");
-    if (h < 24) {
-      if ((lineColor == red)||(lineColor == blue)||(lineColor == orange)||(lineColor == yellow)||(lineColor == green)) {
-        ++endCount;
-        if (endCount >= 5) {
-          ++h;
-          timerCount = 0;
-          document.getElementById("hour").innerHTML = h;
-          draw(red, h);
-          draw(blue, h);
-          draw(orange, h);
-          draw(yellow, h);
-          draw(green, h);
-          endCount = 0;
-        }
-      }
-    }
+    console.log("A single line has finished.");
   }
 }
 
